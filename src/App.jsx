909ea924cdefa15e7b320c33184c1ce4fe8746d6
Dagim Sisay <Dagim>
2025-07-10 18:56:03 +0300

    Completed Meme Generator project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from "react";
 import Header from "./components/Header";
-
-export default function App() {
+import Meme from "./components/Meme";
+const App = () => {
   return (
     <div>
       <Header />
+      <Meme />;
     </div>
   );
-}
+};
+export default App;
